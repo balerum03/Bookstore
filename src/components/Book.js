2fe834e940fd40ba8f65a49c35e0a82db524e37a
Adan Fernandez B <adaferbon03@gmail.com>
2021-04-22 12:29:53 -0500

    add more styles to match with given design

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import progress_circle from '../assets/progress_circle.png'
 
 const Book = props => {
 	const {book, remove} = props;
@@ -8,10 +9,29 @@ const Book = props => {
 			<tr key={book.id}>
 				<div className="book">
 					<div className="book-info">
-						<td className="book-title">{book.title}</td>
 						<td className="book-category">{book.category}</td>
+						<td className="book-title">{book.title}</td>
+						<td className="book-author">Adan Fernandez B.</td>
+						<ul className="book-actions">
+							<li><a>Commets</a></li>
+							<li><button type='button' onClick={remove} value={book.id} className="remove-btn">Remove</button></li>
+							<li><a>Edit</a></li>
+						</ul>
 					</div>
-					<button type='button' onClick={remove} value={book.id} className="btn">Remove</button>
+					<div className="over-all-progress">
+						<div>
+							<img atl="progress circle" src={progress_circle} />
+						</div>
+						<div>
+							<p className="progress-number">64%</p>
+							<p className="completed-text">Completed</p>
+						</div>
+					</div>
+					<div>
+						<p className="chapter-info">CURRENT CHAPTER</p>
+						<p className="chapter-number">CHAPTER 17</p>
+						<button className="btn">UPDATE PROGRESS</button>
+					</div>
 				</div>
 			</tr>
 		</tbody>
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon  } from '@fortawesome/react-fontawesome';
+
 
 const Navigation = () => (
     <nav className="panel-bg">
-        <h1 className="Bookstore-CMS">BookStore CMS</h1>
-        <ul>
-            <li>Books</li>
-            <li>Categories</li>
-        </ul>
+        <div className="nav-title">
+            <h1 className="Bookstore-CMS">BookStore CMS</h1>
+            <a href="#">Books</a>
+            <a href="#">Categories</a>
+        </div>
+        <div>
+            <FontAwesomeIcon icon={faUserCircle} size="lg" className="user-icon"/>
+        </div>
     </nav>
 );
 
